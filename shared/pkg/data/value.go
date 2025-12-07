86dcaaf0ae6c1f36966c7dc239c17b31d70fc556
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-12-07 14:41:36 -0500

    Add value creation

--- shared/pkg/data/value.go
+++ shared/pkg/data/value.go
@@ -1,12 +1,28 @@
 package data
 
 import (
+	"encoding/binary"
+	"errors"
 	"shared/pkg/types"
 )
 
 type Value struct {
 	Type types.TypeCode
 	Data []byte
+}
+
+func NewValue(v any) (Value, error) {
+	switch x := v.(type) {
+	case int64:
+		data := make([]byte, 8)
+		binary.BigEndian.PutUint64(data, uint64(x))
+		return Value{
+			Type: types.LoInt,
+			Data: data,
+		}, nil
+	}
+
+	return Value{}, errors.New("invalid value type")
 }
 
 func (v *Value) GetInt() int64 {
