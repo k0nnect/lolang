6d4a476d39c8ae8d7c998406a06cb590a0604405
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-12-07 15:26:21 -0500

    Add call stack to vm

--- runtime/internal/vm/vmcontext.go
+++ runtime/internal/vm/vmcontext.go
@@ -1,12 +1,15 @@
 package vm
 
 import (
+	vmFunc "runtime/internal/vm/function"
+	"shared/pkg/data"
 	"shared/pkg/function"
 )
 
 type VCtx struct {
 	EntryPoint *function.Function
 	Functions  map[int]function.Function
+	CallStack  data.Stack[*vmFunc.Ctx]
 }
 
 // Error halts the virtual machine and kills the running application
