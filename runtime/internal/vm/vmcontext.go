650a7f2611990829527df7d884b09e9f1f9dcf8f
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-12-07 14:41:43 -0500

    Add vm error

--- runtime/internal/vm/vmcontext.go
+++ runtime/internal/vm/vmcontext.go
@@ -7,3 +7,8 @@ import (
 type VCtx struct {
 	Functions map[int]function.Function
 }
+
+// Error halts the virtual machine and kills the running application
+func (vm *VCtx) Error(err error) {
+	panic(err)
+}
